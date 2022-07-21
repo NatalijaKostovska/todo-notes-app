d1f6e776c1bbc20af583b201598995f4d908afd3
natalija.kostovska <natalija.kostovska@iwconnect.com>
2022-07-21 10:03:55 +0200

    add clock

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 
 function Header() {
+
+    const [time, setTime] = useState();
+
+    const getTime = () => {
+        let today = new Date();
+        setTime(today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds())
+    }
+
+    useEffect(() => {
+        setInterval(() => getTime(), 1000);
+    }, [time])
+
     return (
         <div className="header">
             Dashboard
+            <div className='header-clock'>
+                Clock
+                <div className='clock-box'>
+                    <div className='clock-day-month'><span>day</span> <span>Month</span></div>
+                    <span>{time}</span>
+                </div>
+            </div>
         </div>
     )
 }
